852e4286e0e324d732b71deab3bdc7a943bcc2f3
zeyn13 <likazeynep@gmail.com>
2023-01-18 16:55:16 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,47 @@
 <template>
-  <nav>
+  <Navbar />
+  <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  </div>
+  <router-view />
 </template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import axios from "axios";
+export default {
+  components: { Navbar },
+  data() {
+    return {
+      baseURL: "https://limitless-lake-55070.herokuapp.com/",
+      products: [],
+      categories: [],
+    };
+  },
+  methods: {
+    async fetchData() {
+      // api call to get all the categories
+      await axios
+        .get(this.baseURL + "category/")
+        .then((res) => {
+          this.categories = res.data;
+        })
+        .catch((err) => console.log("err", err));
+      // api call to get the products
+      await axios
+        .get(this.baseURL + "product/")
+        .then((res) => {
+          this.products = res.data;
+        })
+        .catch((err) => console.log("err", err));
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
+</script>
 
 <style>
 #app {
@@ -14,17 +51,14 @@
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
+#nav {
   padding: 30px;
 }
-
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
-nav a.router-link-exact-active {
+#nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
