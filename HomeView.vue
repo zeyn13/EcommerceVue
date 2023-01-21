fa25c82eb8e1ad4c963fb2d059e23c057f45a1e8
Jehona123 <101560137+Jehona123@users.noreply.github.com>
2023-01-21 15:38:37 +0100

    Create HomeView.vue

---
+++ HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+   <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.Vue"; 
+
+  export default {
+    name:"App",
+    components:{
+      HelloWorld,
+      },
+  };
+</script>
+
+</style>
+#app {
+font-family:Avenir,Helvetica,Arial,sans-serif;
